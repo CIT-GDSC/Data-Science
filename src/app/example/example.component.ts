8bd84a1514d0c149859604d5f8a4ae75fd99333d
Domains18 <gibsonsgibson88@gmail.com>
2023-02-18 01:46:22 +0300

    learning angular

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-example',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./example.component.css']
 })
 export class ExampleComponent {
-
+  @Input() name: string = "not shaun";
+  @Input() lastName: string = "not mckinnon";
 }
